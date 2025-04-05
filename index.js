e8621675ff9d55c9a9b0ac24f3d6a2339ddfbaae
Dmitro Adieliev <adelevdmitriy@gmail.com>
2025-04-05 16:21:55 +0300

    added static files support

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 
 const router = require('./routes/index')
 const sequelize = require('./db')
@@ -13,6 +15,8 @@ const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
 app.use('/api', router)
 
 // middlewares
