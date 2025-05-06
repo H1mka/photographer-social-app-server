51d58bc5204713f91bfcf86e8f990bbcac7991ed
Dmitro Adieliev <adelevdmitriy@gmail.com>
2025-05-06 19:25:40 +0300

    added custom timeout middleware

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -2,8 +2,18 @@ const Router = require('express')
 const router = new Router()
 const commentController = require('../controllers/commentController')
 const authMiddleware = require('../middleware/AuthMiddleware')
+const timeoutMiddleware = require('../middleware/TimeoutMiddleware')
 
-router.post('/create', authMiddleware, commentController.create)
-router.get('/getPhotoComments', commentController.getPhotoComments)
+router.post(
+  '/create',
+  authMiddleware,
+  timeoutMiddleware,
+  commentController.create
+)
+router.get(
+  '/getPhotoComments',
+  timeoutMiddleware,
+  commentController.getPhotoComments
+)
 
 module.exports = router
